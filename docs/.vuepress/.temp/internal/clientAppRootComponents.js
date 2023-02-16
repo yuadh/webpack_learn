6e443118f839d412458b8ea77b083d163751820e
yuadh <1912546219@qq.com>
2023-02-16 17:30:55 +0800

    docs

---
+++ docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,5 @@
+import clientAppRootComponent0 from 'H:/code-demo/javaScript/webpack_learn/webpack_docs/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+]
---
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -0,0 +1,15 @@
+export const siteData = {
+  "base": "/webpack5-docs/",
+  "lang": "zh-CN",
+  "title": " Webpack5 教程",
+  "description": "Webpack5从入门到入土",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon"
+      }
+    ]
+  ],
+  "locales": {}
+}
